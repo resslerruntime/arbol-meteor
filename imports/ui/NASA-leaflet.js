be8763a0de71527006c680693051b4601954d657
aturc <adrianturcato@gmail.com>
2018-11-06 22:01:32 -0500

    add leaflet map
    isn't working

--- imports/ui/NASA-leaflet.js
+++ imports/ui/NASA-leaflet.js
@@ -1,3 +1,4 @@
+import * as L from "leaflet";
 import './body.js';
 
 ////////////////////////////////////////////
@@ -80,4 +81,119 @@ function yearlyNASAVals (a,startDate,endDate){
 ////////////////////////////////////////////
 // leaflet map
 ////////////////////////////////////////////
+
+console.log("leaflet",L)
+
+//find the proper way to make sure that the HTML is fully loaded before leaflet tries to attach the map
+var waitForLeaflet = setInterval(function(){
+  let leafletDiv = document.getElementById("leaflet-map");
+  console.log("leaflet Div",leafletDiv)
+  if(leafletDiv){
+    window.clearInterval(waitForLeaflet);
+    var mymap = L.map('leaflet-map').setView([40.712, -74.227], 5);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+      maxZoom: 15,
+      minZoom: 1,
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      id: 'mapbox.streets'
+    }).addTo(mymap);
+  }
+},1000)
+
+
+// var mymap = L.map('leaflet-map').setView([40.712, -74.227], 5);
+
+// L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+//   maxZoom: 15,
+//   minZoom: 1,
+//   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+//     '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+//     'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//   id: 'mapbox.streets'
+// }).addTo(mymap);
+
+// //variables
+// let currentBounds;
+// let selectedBounds;
+// let presentSelection = false;
+
+// //outof bounds
+// let northPole = L.rectangle([[50, -360], [90, 360]], {color:"white",opacity:0.5}).addTo(mymap);
+// let southPole = L.rectangle([[-50, -360], [-90, 360]], {color:"white",opacity:0.5}).addTo(mymap);
+
+// //define shape to be displayed with d3
+// let rect = L.rectangle([[0, 0], [0.25, 0.25]], {color:"blue", className: "mouse"}).addTo(mymap);
+// let viewSVG = d3.selectAll("svg").append("path")
+//   .attr("class","view")
+//   .attr("d", "M333 317L333 309L339 309L339 317z")
+//   .attr("fill", "none")
+//   .attr("fill-opacity",0.1)
+//   .attr("stroke", "none")
+//   .attr("stroke-weight",2);
+
+// function mouseMove(e) {
+//   console.log("mouseMove")
+//   let step = 0.05;
+//   if(rect) mymap.removeLayer(rect);
+//   let f = Math.floor(e.latlng.lat/step), g = Math.floor(e.latlng.lng/step);
+//   let lat = f*step, lng = g*step;
+//   currentBounds = [[lat, lng], [lat+0.25, lng+0.25]];
+//   //draw dummy rectangle for new location
+//   rect = L.rectangle(currentBounds, {className: "mouse"}).addTo(mymap);
+// }
+
+// function mouseClick(e){
+//   let d = getPath("mouse");
+//   if(presentSelection){
+//     console.log("already selected")
+//     viewSVG.transition()
+//       .attr("d",d)
+//       .attr("fill","green")
+//       .attr("stroke","green");
+//   }else{
+//     console.log("no selection")
+//     viewSVG.attr("d",d)
+//       .transition()
+//       .attr("fill","green")
+//       .attr("stroke","green");
+//   }
+//   console.log(currentBounds)
+//   selectedBounds = currentBounds;
+//   presentSelection = true;
+// }
+
+// function mouseZoom(e){
+//   //redraw objects so that they are the right size once zoomed
+//   if(selectedBounds){
+//     let dummy = L.rectangle(selectedBounds, {fill: "none", stroke:"none", className: "dummy"}).addTo(mymap);
+//     let d = getPath("dummy");
+//     if(d === "M0 0"){
+//       presentSelection = false;
+//       //object has been automatically removed
+//       viewSVG.attr("fill","none")
+//         .attr("stroke","none");
+//       mymap.removeLayer(dummy);
+//     }else{
+//       presentSelection = true;
+//       //redraw selected object to new size
+//       viewSVG.attr("d",d)
+//       .attr("fill","green")
+//       .attr("stroke","green");
+//       mymap.removeLayer(dummy);
+//     }
+//     console.log(presentSelection)
+//   }
+// }
+
+// function getPath (className){
+//   var path = document.getElementsByClassName(className);
+//   console.log(className,path,path[0].getAttribute("d"))
+//   return path[0].getAttribute("d");
+// }
+
+// mymap.on('zoom', mouseZoom)
+// mymap.on('mousemove', mouseMove);
+// mymap.on('click', mouseClick);
 
