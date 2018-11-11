bddd6e268a7d8da482cdbdb09b3544cf8f764818
aturc <adrianturcato@gmail.com>
2018-11-10 22:57:49 -0500

    small updates

--- imports/ui/NASA-leaflet.js
+++ imports/ui/NASA-leaflet.js
@@ -1,4 +1,5 @@
 import * as L from "leaflet";
+import * as d3 from "d3";
 import './body.js';
 
 ////////////////////////////////////////////
@@ -10,15 +11,23 @@ let checkStatus;
 
 callNASA = function (startDate,endDate,location){
   $("#error-msg").fadeOut(500);
+  $("#error-msg2").fadeOut(500);
   $("#chart-loader").fadeIn(1000);
   console.log("nasa:",startDate.mmddyyyy,endDate.mmddyyyy)
   //reset call parameters
   submittedDataRequest = false;
   window.clearInterval(checkStatus);
 
+  //get area that was selected
+  // let coords = [[40.55,-83.1],[40.8,-83.1],[40.8,-82.85],[40.55,-82.85]];
+  // let coords = [[40.55,-83.1],[45,-83.1],[45,-80],[40.55,-80]];
+  let coords = [[2.6500000000000004,32.1],[2.9000000000000004,32.1],[2.9000000000000004,32.35],[2.6500000000000004,32.35]];
+  if(selectedBounds) coords = leafletToNasaCoords(selectedBounds);
+  console.log(coords,stringifyCoords(coords));
+
   //submit initial data request
   // Meteor.call("submitDataRequestNASA","04/01/2008","06/30/2018",stringifyCoords([]),function(error, results) {
-  Meteor.call("submitDataRequestNASA",startDate.mmddyyyy,endDate.mmddyyyy,stringifyCoords([]),function(error, results) {
+  Meteor.call("submitDataRequestNASA",startDate.mmddyyyy,endDate.mmddyyyy,stringifyCoords(coords),function(error, results) {
     if(typeof results != 'undefined'){
       console.log("data request NASA",results,results.content)
       let id = eval(results.content)[0];
@@ -36,12 +45,17 @@ callNASA = function (startDate,endDate,location){
             Meteor.call("getDataFromRequestNASA",id,function(error, results) {
               submittedDataRequest = false;
               let d = JSON.parse(results.content)
-              console.log("data NASA",d)
-              let obj = yearlyNASAVals(d.data,startDate,endDate);
-              updateMonths(obj);
-              calcTenYrP(obj);
-              $(".chart-loader-div").removeClass("chart-loader");
-              $("#chart-loader").fadeOut(500);              
+              if(d.data.length !== 0){
+                let obj = yearlyNASAVals(d.data,startDate,endDate);
+                updateMonths(obj);
+                calcTenYrP(obj);
+                $(".chart-loader-div").removeClass("chart-loader");
+                $("#chart-loader").fadeOut(500);  
+              }else{
+                console.log("No data returned")
+                $("#chart-loader").fadeOut(500);
+                $("#error-msg2").fadeIn(1000);
+              }            
             });
           }
         });
@@ -54,13 +68,21 @@ callNASA = function (startDate,endDate,location){
   });
 }
 
-function stringifyCoords (aa){
-  let a = [[21.5,-3.1],[21.5,-6.4],[26.2,-5.4],[26.1,-2.6]];
+function stringifyCoords (a){
   let s = "[";
   a.map(d => s += `[${d[0]},${d[1]}],`);
   s = s.substring(0, s.length - 1);
   s += "]";
   return s;
+}
+
+function leafletToNasaCoords(a){
+  return [
+    [a[0][0],a[0][1]]
+    ,[a[0][0]+0.25,a[0][1]]
+    ,[a[0][0]+0.25,a[0][1]+0.25]
+    ,[a[0][0],a[0][1]+0.25]
+  ];
 }
 
 var months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec","Jan"];
@@ -94,13 +116,16 @@ function yearlyNASAVals (a,startDate,endDate){
 // leaflet map
 ////////////////////////////////////////////
 
-console.log("leaflet",L);
 regionmap = false;
+
+//variables
+let currentBounds;
+let selectedBounds;
+let presentSelection = false;
 
 //find the proper way to make sure that the HTML is fully loaded before leaflet tries to attach the map
 var waitForLeaflet = setInterval(function(){
   let leafletDiv = document.getElementById("mapdiv");
-  console.log("leaflet Div",leafletDiv)
   if(leafletDiv){
     window.clearInterval(waitForLeaflet);
     regionmap = L.map('mapdiv').setView([40.712, -74.227], 5);
@@ -113,102 +138,95 @@ var waitForLeaflet = setInterval(function(){
       id: 'mapbox.streets'
     }).addTo(regionmap);
 
-    console.log('regionmap is now ' + typeof regionmap);
+    // console.log('regionmap is now ' + typeof regionmap);
+
+    //outof bounds
+    let northPole = L.rectangle([[50, -360], [90, 360]], {color:"white",opacity:0.5}).addTo(regionmap);
+    let southPole = L.rectangle([[-50, -360], [-90, 360]], {color:"white",opacity:0.5}).addTo(regionmap);
+
+    // //define shape to be displayed with d3
+    let rect = L.rectangle([[0, 0], [0.25, 0.25]], {color:"blue", className: "mouse"}).addTo(regionmap);
+    let viewSVG = d3.selectAll("#mapdiv")
+      .selectAll("svg")
+      .append("path")
+      .attr("d", "M333 317L333 309L339 309L339 317z")
+      .attr("fill", "none")
+      .attr("fill-opacity",0.1)
+      .attr("stroke", "none")
+      .attr("stroke-weight",2);
+
+    function mouseMove(e) {
+      // console.log("mouseMove")
+      let step = 0.05;
+      if(rect) regionmap.removeLayer(rect);
+      let f = Math.floor(e.latlng.lat/step), g = Math.floor(e.latlng.lng/step);
+      let lat = f*step, lng = g*step;
+      currentBounds = [[lat, lng], [lat+0.25, lng+0.25]];
+      //draw dummy rectangle for new location
+      rect = L.rectangle(currentBounds, {className: "mouse"}).addTo(regionmap);
+    }
+
+    function mouseClick(e){
+      let d = getPath("mouse");
+      if(presentSelection){
+        // console.log("already selected")
+        viewSVG.transition()
+          .attr("d",d)
+          .attr("fill","green")
+          .attr("stroke","green");
+      }else{
+        // console.log("no selection")
+        viewSVG.attr("d",d)
+          .transition()
+          .attr("fill","green")
+          .attr("stroke","green");
+      }
+      selectedBounds = currentBounds;
+      presentSelection = true;
+    }
+
+    function mouseZoom(e){
+      //redraw objects so that they are the right size once zoomed
+      if(selectedBounds){
+        let dummy = L.rectangle(selectedBounds, {fill: "none", stroke:"none", className: "dummy"}).addTo(regionmap);
+        let d = getPath("dummy");
+        if(d === "M0 0"){
+          presentSelection = false;
+          //object has been automatically removed
+          viewSVG.attr("fill","none")
+            .attr("stroke","none");
+          regionmap.removeLayer(dummy);
+        }else{
+          presentSelection = true;
+          //redraw selected object to new size
+          viewSVG.attr("d",d)
+          .attr("fill","green")
+          .attr("stroke","green");
+          regionmap.removeLayer(dummy);
+        }
+        // console.log(presentSelection)
+      }
+    }
+
+    function getPath (className){
+      var path = document.getElementsByClassName(className);
+      // console.log(className,path,path[0].getAttribute("d"))
+      return path[0].getAttribute("d");
+    }
+
+    regionmap.on('zoom', mouseZoom)
+    regionmap.on('mousemove', mouseMove);
+    regionmap.on('click', mouseClick);
   }
-},1000)
+},1000);
+
+getSelectedBounds = function () {
+  return selectedBounds;
+}
 
 
-// var mymap = L.map('leaflet-map').setView([40.712, -74.227], 5);
 
-// L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-//   maxZoom: 15,
-//   minZoom: 1,
-//   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-//     '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-//     'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//   id: 'mapbox.streets'
-// }).addTo(mymap);
 
-// //variables
-// let currentBounds;
-// let selectedBounds;
-// let presentSelection = false;
 
-// //outof bounds
-// let northPole = L.rectangle([[50, -360], [90, 360]], {color:"white",opacity:0.5}).addTo(mymap);
-// let southPole = L.rectangle([[-50, -360], [-90, 360]], {color:"white",opacity:0.5}).addTo(mymap);
 
-// //define shape to be displayed with d3
-// let rect = L.rectangle([[0, 0], [0.25, 0.25]], {color:"blue", className: "mouse"}).addTo(mymap);
-// let viewSVG = d3.selectAll("svg").append("path")
-//   .attr("class","view")
-//   .attr("d", "M333 317L333 309L339 309L339 317z")
-//   .attr("fill", "none")
-//   .attr("fill-opacity",0.1)
-//   .attr("stroke", "none")
-//   .attr("stroke-weight",2);
-
-// function mouseMove(e) {
-//   console.log("mouseMove")
-//   let step = 0.05;
-//   if(rect) mymap.removeLayer(rect);
-//   let f = Math.floor(e.latlng.lat/step), g = Math.floor(e.latlng.lng/step);
-//   let lat = f*step, lng = g*step;
-//   currentBounds = [[lat, lng], [lat+0.25, lng+0.25]];
-//   //draw dummy rectangle for new location
-//   rect = L.rectangle(currentBounds, {className: "mouse"}).addTo(mymap);
-// }
-
-// function mouseClick(e){
-//   let d = getPath("mouse");
-//   if(presentSelection){
-//     console.log("already selected")
-//     viewSVG.transition()
-//       .attr("d",d)
-//       .attr("fill","green")
-//       .attr("stroke","green");
-//   }else{
-//     console.log("no selection")
-//     viewSVG.attr("d",d)
-//       .transition()
-//       .attr("fill","green")
-//       .attr("stroke","green");
-//   }
-//   console.log(currentBounds)
-//   selectedBounds = currentBounds;
-//   presentSelection = true;
-// }
-
-// function mouseZoom(e){
-//   //redraw objects so that they are the right size once zoomed
-//   if(selectedBounds){
-//     let dummy = L.rectangle(selectedBounds, {fill: "none", stroke:"none", className: "dummy"}).addTo(mymap);
-//     let d = getPath("dummy");
-//     if(d === "M0 0"){
-//       presentSelection = false;
-//       //object has been automatically removed
-//       viewSVG.attr("fill","none")
-//         .attr("stroke","none");
-//       mymap.removeLayer(dummy);
-//     }else{
-//       presentSelection = true;
-//       //redraw selected object to new size
-//       viewSVG.attr("d",d)
-//       .attr("fill","green")
-//       .attr("stroke","green");
-//       mymap.removeLayer(dummy);
-//     }
-//     console.log(presentSelection)
-//   }
-// }
-
-// function getPath (className){
-//   var path = document.getElementsByClassName(className);
-//   console.log(className,path,path[0].getAttribute("d"))
-//   return path[0].getAttribute("d");
-// }
-
-// mymap.on('zoom', mouseZoom)
-// mymap.on('mousemove', mouseMove);
-// mymap.on('click', mouseClick);
 
